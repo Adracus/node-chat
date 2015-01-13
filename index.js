46589f865c149b496ef362460fb5d36346bbf450
unknown <d059357@QKAN00288563A.emea.global.corp.sap>
2015-01-13 11:04:30 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var messages = [];
+
+var ChatMessage = function(user, text) {
+	this.user = user || "Anonymous";
+	this.text = text || "";
+	this.timestamp = new Date();
+};
+
+
+// Middleware
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+// Routes
+app.get("/messages", function(req, res) {
+	return res.send(messages);
+});
+
+app.post("/messages", function(req, res) {
+	var user = req.body.user;
+	var text = req.body.text;
+
+	var chatMessage = new ChatMessage(user, text);
+	messages.push(chatMessage);
+	io.emit("chat-message", chatMessage);
+
+	return res.status(201).send(chatMessage);
+});
+
+io.on("connection", function() {
+});
+
+var server = http.listen(process.env.PORT || 3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+
+  	console.log("Chat app listening at http://%s:%s", host, port);
+});
---
+++ public/script.js
@@ -0,0 +1,86 @@
+"use strict";
+
+var socket = io();
+
+var chatApp = angular.module("chat", []);
+
+var Message = function(user, text, timestamp) {
+	this.user = user || "Anonymous";
+	this.text = Message.encodeText(text || "");
+	this.timestamp = timestamp || new Date();
+
+	this.timestampString = function() {
+		return moment(this.timestamp).fromNow();
+	};
+
+};
+
+Message.parse = function(message) {
+	var user = message.user;
+	var text = message.text;
+	var timestamp = message.timestamp;
+
+	return new Message(user, text, timestamp);
+};
+
+Message.encodeText = function(text) {
+	var words = text.split(" ");
+	return words.map(function(word) {
+		if (word.isUrl()) {
+			if (word.isImage()) return "<img src=\"" + word + "\" />";
+			return "<a href=\"" + word + "\">" + word + "</a>";
+		}
+		return word;
+	}).join(" ");
+};
+
+chatApp.controller("MessageListCtrl", function($scope, $http, $sce) {
+	$scope.messages = [];
+	$scope.sce = $sce;
+
+	var scrollDown = function() {
+		setTimeout(function() {
+			var height = (document.height !== undefined) ? document.height : document.body.offsetHeight;
+			window.scrollBy(0, height);
+		}, 100);
+	};
+
+	$http.get("/messages").success(function(messages) {
+		$scope.messages = messages.map(Message.parse);
+		scrollDown();
+	});
+
+	socket.on("chat-message", function(message) {
+		$scope.messages.push(Message.parse(message));
+		scrollDown();
+	});
+
+	$scope.send = function() {
+		var text = $scope.newMessage;
+		var user = $scope.user || "Anonymous";
+
+		var message = {text: text, user: user};
+
+		$http.post("/messages", message).success(function() {
+			console.log("message successfully delivered");
+		});
+
+		$scope.newMessage = "";
+	};
+});
+
+String.prototype.isUrl = function() {
+	return /^(ftp|http|https):\/\/[^ "]+$/.test(this)
+}
+
+String.prototype.endsWith = function(suffix) {
+	return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+String.prototype.isImage = function() {
+	var self = this;
+	var extensions = [".gif", ".png", ".jpg", ".jpeg"];
+	return extensions.some(function(extension) {
+		return self.endsWith(extension);
+	});
+}
